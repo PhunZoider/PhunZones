318ab1bbcebfd40dc6665ebc13494d27c139813b
PhunZoider <projectphunzoid@gmail.com>
2026-02-23 07:06:50 +0000

    refactor struct

---
+++ Contents/mods/PhunZones2/common/media/lua/shared/PhunZones/data.lua
@@ -0,0 +1,181 @@
+return {
+    ["_default"] = {
+        title = "Kentucky",
+        difficulty = 2
+    },
+    void = {
+        difficulty = 0,
+        title = "Void",
+        isVoid = true,
+        zeds = false,
+        bandits = false,
+        rv = true,
+        points = {{21000, 6000, 100000, 100000}}
+    },
+    Very_Easy = {
+        difficulty = 0
+    },
+    Easy = {
+        difficulty = 1
+    },
+    Medium = {
+        difficulty = 2
+    },
+    Hard = {
+        difficulty = 3
+    },
+    Very_Hard = {
+        difficulty = 4
+    },
+    WestPoint = {
+        inherits = "Medium",
+        title = "West Point",
+        points = {{11100, 6580, 13199, 7499}}
+    },
+    EchoPark = {
+        inherits = "Medium",
+        title = "Echo Park",
+        points = {{3340, 10818, 3921, 11410}}
+    },
+    Ekron = {
+        inherits = "Medium",
+        title = "Ekron",
+        points = {{10, 9226, 1101, 9974}}
+    },
+    ErikasFurnitureStore = {
+        inherits = "Hard",
+        title = "Erikas",
+        modsRequired = "\\Erikas_Furniture_Store",
+        points = {{11490, 8235, 11578, 8323}}
+    },
+    Grapeseed = {
+        inherits = "Medium",
+        title = "Grapeseed",
+        modsRequired = "\\42Grapeseed",
+        points = {{6280, 10800, 7488, 11693}}
+    },
+    Irvington = {
+        inherits = "Medium",
+        title = "Irvington",
+        points = {{2155, 13785, 3013, 14245}, {1691, 14246, 3149, 14980}}
+    },
+    IrvingtonSpeedway = {
+        inherits = "Medium",
+        title = "Irvington",
+        subtitle = "Speedway",
+        points = {{870, 12770, 1234, 13424}}
+    },
+    MarchRidge = {
+        inherits = "Medium",
+        title = "March Ridge",
+        points = {{9600, 12600, 10499, 13199}}
+    },
+    Muldraugh = {
+        inherits = "Easy",
+        title = "Muldraugh",
+        points = {{10558, 9175, 11023, 10692}}
+    },
+    Rosewood = {
+        inherits = "Very_Easy",
+        title = "Rosewood",
+        points = {{7800, 10800, 8699, 12299}}
+    },
+    Rosewood_Cabins = {
+        points = {{7503, 11402, 7788, 11689}},
+        inherits = "Rosewood",
+        subtitle = "Cabins",
+        modsRequired = "\\rosewoodcabins"
+    },
+    Rosewood_Prison = {
+        points = {{7559, 11741, 7786, 11983}},
+        inherits = "Rosewood",
+        difficulty = 3,
+        subtitle = "State Prison"
+    },
+    Rosewood_Prison2 = {
+        points = {{7559, 11741, 7786, 11983}},
+        inherits = "Rosewood",
+        subtitle = "State Prison",
+        difficulty = 3,
+        modsRequired = "\\rosewood_prison"
+    },
+    Rosewood_Mall = {
+        points = {{7560, 11420, 7777, 11637}},
+        inherits = "Rosewood",
+        subtitle = "Mall",
+        modsRequired = "\\Rosewood Mall"
+    },
+    Riverside = {
+        inherits = "Medium",
+        title = "Riverside",
+        points = {{5400, 5100, 6899, 5699}}
+    },
+    Louisville = {
+        inherits = "Hard",
+        title = "Louisville",
+        points = {{12400, 3904, 12545, 4483}, {11700, 950, 15954, 4215}}
+    },
+    Louisville_Airport = {
+        points = {{15251, 2418, 15684, 3346}},
+        subtitle = "Airport",
+        inherits = "Louisville",
+        difficulty = 4
+    },
+    Louisville_Trainyard = {
+        points = {{12607, 4196, 12859, 4495}, {12548, 4355, 12859, 4495}},
+        subtitle = "Trainyard",
+        inherits = "Louisville"
+    },
+    Louisville_Mall = {
+        points = {{13519, 5724, 14088, 5975}},
+        inherits = "Louisville",
+        subtitle = "Mall",
+        difficulty = 4
+    },
+    Louisville_Quarantine_Zone = {
+        points = {{13414, 3957, 13978, 4193}},
+        subtitle = "Quarantine Zone",
+        inherits = "Louisville",
+        modsRequired = "\\Louisville_Quarantine_Zone",
+        difficulty = 4
+    },
+    Louisville_Riverboat = {
+        points = {{13084, 1165, 13146, 1199}},
+        inherits = "Louisville",
+        subtitle = "Riverboat",
+        modsRequired = "\\Louisville_Riverboat"
+    },
+    Taylorsville = {
+        inherits = "Medium",
+        title = "Taylorsville",
+        modsRequired = "\\Taylorsville",
+        points = {{9302, 6603, 10194, 7130}}
+    },
+    tikitown = {
+        inherits = "Medium",
+        title = "Tikitown",
+        modsRequired = "\\tikitown",
+        points = {{6889, 7188, 7386, 7741}, {7199, 6900, 7796, 7799}}
+    },
+    ValleyStation = {
+        inherits = "Hard",
+        title = "Valley Station",
+        points = {{12397, 4331, 14737, 6477}}
+    },
+    Brandenburg = {
+        inherits = "Hard",
+        title = "Brandenburg",
+        points = {{814, 5384, 3100, 7066}}
+    },
+    FallasLake = {
+        inherits = "Medium",
+        title = "Fallas Lake",
+        points = {{7010, 8090, 7448, 8550}}
+    },
+    Frogtown = {
+        inherits = "Medium",
+        title = "Frogtown",
+        modsRequired = "\\Frogtown",
+        points = {{3300, 7800, 3800, 7500}}
+    }
+}
